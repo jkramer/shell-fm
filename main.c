4b21c6c0b516f3bdf6ae828a28559bfc62809b3a
lizer <lizer@7df44517-d413-0410-91cf-82ca28b36b55>
2006-05-15 16:02:38 +0000

    Recorrected typo. :)
    git-svn-id: svn://nex.scrapping.cc/shell-fm/trunk@10 7df44517-d413-0410-91cf-82ca28b36b55

--- main.c
+++ main.c
@@ -47,7 +47,7 @@ unsigned
 
 int main(int argc, char ** argv) {
 	puts("Shell.FM v" VERSION ", written 2006 by Jonas Kramer");
-	puts("Publicized under the terms of the GNU General Public License (GPL)");
+	puts("Published under the terms of the GNU General Public License (GPL)");
 	
 	if(argc > 2) {
 		fprintf(stderr, "usage: %s [lastfm://...]\n", * argv);
