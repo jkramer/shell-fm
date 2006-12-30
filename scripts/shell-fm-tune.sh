be85e3588a5bef4b66d0b6b37c62c561e54dd625
jkr <jkr@7df44517-d413-0410-91cf-82ca28b36b55>
2006-12-30 15:45:12 +0000

    The tune bash script now tries to read the shell-fm server config from ~/.shell-fm/shell-fm.rc if IP & PORT are not given as command line arguments.
    git-svn-id: svn://nex.scrapping.cc/shell-fm/trunk@167 7df44517-d413-0410-91cf-82ca28b36b55

--- scripts/shell-fm-tune.sh
+++ scripts/shell-fm-tune.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
+# vim:syntax=sh tabstop=4
 
-IP=127.0.0.1
-PORT=54311
-RADIO=$1
-CMD=telnet
+extract ()
+{
+	KEY="$1"
+	VALUE="$(
+		sed -e "s/^$KEY\s*=\s*//" <<<"`
+			grep -oE "$KEY[ ]*=[ ]*[^ ]+" $HOME/.shell-fm/shell-fm.rc
+		`"
+	)"
+	echo "$VALUE"
+}
 
-if [ -z "$RADIO" ] ; then
-    echo "Usage: shell-fm-tune lastfm://radio_url"
-    exit
+
+if [ $# -eq 0 ]; then
+    echo "Usage: shell-fm-tune lastfm://radio_url [HOST [PORT]]"
+    exit -1
 fi
+
+
+RADIO="$1"
+
+IP=
+PORT=
+
+
+if [ $# -gt 1 ]; then
+	IP="$2"
+	if [ $# -gt 2 ]; then PORT="$3"; fi
+elif [ -r "$HOME/.shell-fm/shell-fm.rc" ]; then
+	IP="`extract "bind"`"
+	PORT="`extract "port"`"
+fi
+
+
+RADIO="$1"
+CMD="telnet" # What about netcat?
+
+
+[ -z "$IP" ] && IP="127.0.0.1"
+[ -z "$PORT" ] && PORT="54311"
+ 
+
 echo "play $RADIO" | $CMD $IP $PORT >/dev/null 2>&1
