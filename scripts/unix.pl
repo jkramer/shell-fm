5ef90b48ca3481bc08ed623fc9ebceac91d3b1d2
Jonas Kramer <jkramer@nex.scrapping.cc>
2008-05-20 23:54:09 +0200

    Added sample script for accessing shell-fm via the new UNIX socket interface.

---
+++ scripts/unix.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+# Shell.FM UNIX Socket Control
+# Copyright (C) by Jonas Kramer. All rights reserved.
+# Published under the terms of the GNU General Public License (GPL).
+#
+#   Usage: $0 <command>
+#
+# See the shell-fm(1) manual for a list of commands.
+
+
+use strict;
+use warnings;
+
+use IO::Socket::UNIX;
+use IO::File;
+
+
+die "Usage: $0 <command>\n" unless @ARGV;
+
+
+my $rc = new IO::File("$ENV{HOME}/.shell-fm/shell-fm.rc");
+
+die "Can't open configuration. $!.\n" unless $rc;
+
+
+my $path;
+
+for($rc->getlines) {
+	$path = $1 if /^\s*unix\s*=\s*([^#\s]+)/;
+}
+
+$rc->close;
+
+die "No socket path found.\n" unless $path;
+
+
+my $socket = new IO::Socket::UNIX($path);
+
+die "Failed to create socket. $!.\n" unless $socket;
+
+$socket->say("@ARGV\n") or die("$!\n");
