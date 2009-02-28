955992c4a47707393cb257080b914de867539011
Jonas Kramer <jkramer@nex.scrapping.cc>
2009-02-28 10:34:29 +0100

    Added own strcasestr for systems that don't have one (dreambox).

--- source/include/util.h
+++ source/include/util.h
@@ -5,6 +5,7 @@
 extern char ** slurp(const char *);
 extern char ** uniq(char **);
 extern int grep(char **, char *);
+extern char * strcasestr(const char *, const char *);
 
 #if ((defined(__FreeBSD__) || defined(__OpenBSD__)) && !defined(__STRNDUP__))
 #define __STRNDUP__
@@ -12,3 +13,4 @@ extern char * strndup(const char *, size_t);
 #endif
 
 #endif
+
