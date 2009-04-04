b1b60ab4939fb754900a173664736e3b29cab659
Matthew Donoughe <mdonoughe@gmail.com>
2009-04-04 16:19:03 +0000

    add Darwin to the list of systems needing strndup

--- source/include/util.h
+++ source/include/util.h
@@ -7,7 +7,7 @@ extern char ** uniq(char **);
 extern int grep(char **, char *);
 extern char * strcasestr(const char *, const char *);
 
-#if ((defined(__FreeBSD__) || defined(__OpenBSD__)) && !defined(__STRNDUP__))
+#if ((defined(__FreeBSD__) || defined(__OpenBSD__)) || defined(__darwin__) && !defined(__STRNDUP__))
 #define __STRNDUP__
 extern char * strndup(const char *, size_t);
 #endif
